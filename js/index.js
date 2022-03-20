b23b37074bbe726e78fee411e320d2068ddcf277
klinglerh <92396093+klinglerh@users.noreply.github.com>
2022-03-20 16:47:18 -0500

    Homework 3 Side Nav
    Side Navigation button works when clicked

---
+++ js/index.js
@@ -0,0 +1,23 @@
+console.log("Your index.js file has loaded!");
+
+$(document).ready(function () {
+    $(".hamburger-lines").click(function () {
+        $("#hamburger-1").toggleClass("is-active");
+        if ($("#hamburger-1").hasClass("is-active")) {
+            $("ul.menu-items").css("display", "flex");
+            console.log("Eat me!");
+        }
+        else {
+            $("ul.menu-items").css("display", "none");
+            console.log("Don't eat me!");
+        }
+
+    });
+    /*
+    window.addEventListener('resize', function (event)  { console.log("Big Sandwich");
+        if ($(window).width() > 1225) {
+            $("ul.menu-items").css("display", "flex");
+            $("#hamburger-1").removeClass("is-active");
+        }
+    }, true); */
+});
